526db484b56d58bdc60dbf7ce7bb86198ffbb7a2
Robert Smith - ASUS Dtop <konokhead@gmail.com>
2018-06-20 17:39:35 -0500

    Completed tutorial

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,19 @@
 import { getInitialData } from '../utils/api';
-import { receiveUsers } from '../action/users';
-import { receiveTweets } from '../action/tweets';
-import { setAuthedUser } from '../action/authedUser';
+import { receiveUsers } from '../actions/users';
+import { receiveTweets } from '../actions/tweets';
+import { setAuthedUser } from '../actions/authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading'
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
     .then(({ users, tweets }) => {
       dispatch(receiveUsers(users))
       dispatch(receiveTweets(tweets))
       dispatch(setAuthedUser(AUTHED_ID))
+      dispatch(hideLoading())
     })
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
+import LoadingBar from 'react-redux-loading'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
-      <div>
-        Starter Code
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            { this.props.loading === true
+              ? null
+              : <div>
+                <Route path='/' exact component={Dashboard} />
+                <Route path='/tweet/:id' exact component={TweetPage} />
+                <Route path='/new' exact component={NewTweet} />
+              </div>
+            }
+          </div>
+        </Fragment>
+        </Router>
     )
   }
 }
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
 
-export default App
+export default connect(mapStateToProps)(App)
